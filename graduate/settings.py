1f377e196fd078c61bf49ac0ed8eba87f779fa3b
DongHyun <danny001006@naver.com>
2022-12-08 18:34:18 +0900

    add sing_up

--- graduate/settings.py
+++ graduate/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #서드파티
+    'bootstrap4',
     #장고 앱
     'users',
 ]
@@ -111,6 +113,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'graduate', 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
